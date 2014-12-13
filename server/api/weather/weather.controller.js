b228b9104f2e7e3f34b17a82a5293f42b97492f6
Peter <petervankoughnett@gmail.com>
2014-12-12 19:07:27 -0600

    Added some psuedocode to weather controllers and changed the APIKEY

--- server/api/weather/weather.controller.js
+++ server/api/weather/weather.controller.js
@@ -9,10 +9,12 @@ exports.index = function(req, res) {
   //   if(err) { return handleError(res, err); }
   //   return res.json(200, weathers);
   // });
-
-  var geoLookupUrl = "http://api.wunderground.com/api/68fc40cc59b03c11/geolookup/q/autoip.json";
-  var MinneapolisLookupUrl = "http://api.wunderground.com/api/68fc40cc59b03c11/geolookup/q/Minneapolis Crystal.json";
-  var demoUrl = 'http://api.wunderground.com/api/68fc40cc59b03c11/conditions/q/CA/San_Francisco.json'
+var apikey = f0978770121b8234
+    
+  var geoLookupUrl = "http://api.wunderground.com/api/"+apikey+"/geolookup/q/autoip.json";
+  var MinneapolisLookupUrl = "http://api.wunderground.com/api/"+apikey+"/geolookup/q/Minneapolis Crystal.json";
+  var demoUrl = "http://api.wunderground.com/api/f0978770121b8234/"+apikey+"/q/CA/San_Francisco.json"
+  var hourlyLookup = "http://api.wunderground.com/api/f0978770121b8234/"+apikey+"/q/CA/San_Francisco.json"
   request(geoLookupUrl,function(request,response){
 
     console.log('data: ', response.body)
@@ -28,6 +30,8 @@ exports.show = function(req, res) {
     return res.json(weather);
   });
 };
+
+
 
 // Creates a new weather in the DB.
 exports.create = function(req, res) {
@@ -62,6 +66,23 @@ exports.destroy = function(req, res) {
     });
   });
 };
+
+//extract information 
+    //go into WUNDERGROUND API, get estimated closest station, get tempterature forecast for the next 16 hours, make array [temp.hr1, temp. hr2...]
+
+//calculate time from array and diameter
+    // r= (1/2)d (convert to centimeters)
+    // V = (4/3)pi r^2 (cm^3)
+    // SA = 4 pi r^2 (cm)
+    // Weight = .9997 * V  (g)
+    // T start = 13 deg C (est. tap water temperature)
+    // T end = 0 deg C
+    // ^Q per second = T start * 4.186 * weight  (joules)
+    // h = 3.5 (adjustable heat constant)
+ calculates temp change over an hour// for each hour, calculate ^Q hourly =3600(Tcurrent - Tambient)(h)(SA/10000) then take Tcurrent = Tcurrent- ^Qhourly/(4.186*weight
+// when Tcurrent <0, take currenthour - currenthour(Tstart)/(Tstart-Tcurrent) = time to freeze completely
+// take 70% of time to freeze completely
+
 
 function handleError(res, err) {
   return res.send(500, err);
